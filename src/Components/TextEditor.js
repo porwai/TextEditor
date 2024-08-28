6a9e253676cc97f2d0991ec222364ed4b8663f85
porwai <porwaiwit@gmail.com>
2024-08-28 16:57:47 +0700

    better interface

--- src/Components/TextEditor.js
+++ src/Components/TextEditor.js
@@ -1,6 +1,26 @@
 import { useCallback } from "react";
 import { Editor, Transforms, Text } from "slate";
 import { Editable } from "slate-react";
+import { IconButton } from "@mui/material";
+import {
+  FormatBold,
+  FormatItalic,
+  FormatUnderlined,
+  Code,
+  FormatQuote
+} from "@mui/icons-material";
+
+const BlockQuote = (props) => {
+  return <blockquote {...props.attributes}>{props.children}</blockquote>;
+}
+
+const CodeElement = (props) => {
+  return <code {...props.attributes}>{props.children}</code>;
+};
+
+const DefaultElement = (props) => {
+  return <p {...props.attributes}>{props.children}</p>;
+};
 
 const Leaf = (props) => {
   return (
@@ -18,15 +38,37 @@ const Leaf = (props) => {
 };
 
 function TextEditor({ editor }) {
+  const renderElement = useCallback((props) => {
+    switch (props.element.type) {
+      case "code":
+        return <CodeElement {...props} />;
+      case 'quote':
+        return <BlockQuote {...props} />;
+      default:
+        return <DefaultElement {...props} />;
+    }
+  }, []);
+
   function changeMark(mark) {
     const [match] = Editor.nodes(editor, {
-      match: (n) => n[mark] // check for existing formatting
+      match: (n) => n[mark]
     });
 
     Transforms.setNodes(
       editor,
-      { [mark]: !match }, // sets the formatting value
+      { [mark]: !match },
       { match: (n) => Text.isText(n), split: true }
+    );
+  }
+
+  function changeType(type) {
+    const [match] = Editor.nodes(editor, {
+      match: (n) => n.type === type
+    });
+    Transforms.setNodes(
+      editor,
+      { type: match ? null : type },
+      { match: (n) => Editor.isBlock(editor, n) }
     );
   }
 
@@ -39,7 +81,7 @@ function TextEditor({ editor }) {
       return;
     }
 
-    event.preventDefault(); // prevents default browser behaviour
+    event.preventDefault();
 
     switch (event.key) {
       case "b": {
@@ -61,7 +103,64 @@ function TextEditor({ editor }) {
       }
     }
   };
-  return <Editable onKeyDown={onKeyDown} renderLeaf={renderLeaf} />;
+  return (
+    <div
+      style={{
+        backgroundColor: "#171718",
+        color: "#fff",
+        textAlign: "start",
+        padding: "10px"
+      }}
+    >
+      <div style={{ display: `flex`, backgroundColor: "#171718" }}>
+        <IconButton
+          style={{ color: "grey" }}
+          onPointerDown={(e) => {
+            changeMark("bold");
+          }}
+        >
+          <FormatBold />
+        </IconButton>
+        <IconButton
+          style={{ color: "grey" }}
+          onPointerDown={(e) => {
+            changeMark("italic");
+          }}
+        >
+          <FormatItalic />
+        </IconButton>
+        <IconButton
+          style={{ color: "grey" }}
+          onPointerDown={(e) => {
+            changeMark("underline");
+          }}
+        >
+          <FormatUnderlined />
+        </IconButton>
+        <IconButton
+          onPointerDown={(e) => {
+            changeType("code");
+          }}
+          style={{ color: "grey" }}
+        >
+          <Code />
+        </IconButton>
+        <IconButton
+          onPointerDown={(e) => {
+            changeType("quote");
+          }}
+          style={{ color: "grey" }}
+        >
+          <FormatQuote />
+        </IconButton>
+      </div>
+      <Editable
+        onKeyDown={onKeyDown}
+        renderLeaf={renderLeaf}
+        renderElement={renderElement}
+      />
+    </div>
+  );
 }
 
 export default TextEditor;
