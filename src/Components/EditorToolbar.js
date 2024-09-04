61c3d9dd6e6fce5141d2dff297fe94c2b0998a43
porwai <porwaiwit@gmail.com>
2024-09-04 15:28:25 +0700

    added components looks

--- src/Components/EditorToolbar.js
+++ src/Components/EditorToolbar.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { IconButton } from "@mui/material";
-import { FormatBold, FormatItalic, FormatUnderlined, Code, FormatQuote } from "@mui/icons-material";
+import { FormatBold, FormatItalic, FormatUnderlined, Code, FormatQuote} from "@mui/icons-material";
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import FormatListNumberedIcon from '@mui/icons-material/FormatListNumbered';
+import FormatAlignLeftIcon from '@mui/icons-material/FormatAlignLeft';
+import FormatAlignRightIcon from '@mui/icons-material/FormatAlignRight';
+import FormatAlignCenterIcon from '@mui/icons-material/FormatAlignCenter';
+import FormatAlignJustifyIcon from '@mui/icons-material/FormatAlignJustify';
+import LooksOneIcon from '@mui/icons-material/LooksOne';
+import LooksTwoIcon from '@mui/icons-material/LooksTwo';
 import { useSlate } from 'slate-react';
 import { isMarkActive, toggleMark, isBlockActive, toggleBlock } from "../utils/editorUtils";
 
@@ -41,7 +49,15 @@ const EditorToolbar = () => {
       <MarkButton format="italic" icon={<FormatItalic />} />
       <MarkButton format="underline" icon={<FormatUnderlined />} />
       <MarkButton format="code" icon={<Code />} />
+      <BlockButton format="heading-one" icon={<LooksOneIcon />} />
+      <BlockButton format="heading-two" icon={<LooksTwoIcon />}/>
       <BlockButton format="block-quote" icon={<FormatQuote />} />
+      <BlockButton format="numbered-list" icon={<FormatListNumberedIcon />} />
+      <BlockButton format="bulleted-list" icon={<FormatListBulletedIcon />} />
+      <BlockButton format="left" icon={<FormatAlignLeftIcon />} />
+      <BlockButton format="center" icon={<FormatAlignCenterIcon />} />
+      <BlockButton format="right" icon={<FormatAlignRightIcon />} />
+      <BlockButton format="justify" icon={<FormatAlignJustifyIcon />} />
     </div>
   );
 };
